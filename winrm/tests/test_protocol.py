3f7d5500f00ef7469fd9976027f7d7fd15bdf32f
Octavio Schroeder <28765504+oschroeder8@users.noreply.github.com>
2025-04-27 13:10:53 -0500

    Feature: Add support for handling connection retries
    Dag Wieers originally submitted this work as a PR in Oct 2018. Due to
    the amount of time that has passed, the PR needs to be rebased. Dag
    has not been able to get back to this.
    I am therefore submitting a new PR with his proposed changes, and added
    some tests as well.
    
    Original PR from Dag:
    https://github.com/diyan/pywinrm/pull/174

--- winrm/tests/test_protocol.py
+++ winrm/tests/test_protocol.py
@@ -81,10 +81,22 @@ def test_send_command_input(protocol_fake):
 
 
 def test_set_timeout_as_sec():
-    protocol = Protocol("endpoint", username="username", password="password", read_timeout_sec="30", operation_timeout_sec="29")
+    protocol = Protocol("endpoint",
+                        username="username",
+                        password="password",
+                        read_timeout_sec="30",
+                        operation_timeout_sec="29")
     assert protocol.read_timeout_sec == 30
     assert protocol.operation_timeout_sec == 29
 
+def test_set_retry_connection():
+    protocol = Protocol("endpoint",
+                        username="username",
+                        password="password",
+                        reconnection_retries="5",
+                        reconnection_backoff="3")
+    assert protocol.reconnection_retries == 5
+    assert protocol.reconnection_backoff == 3.0
 
 def test_fail_set_read_timeout_as_sec():
     with pytest.raises(ValueError) as exc:
@@ -96,3 +108,13 @@ def test_fail_set_operation_timeout_as_sec():
     with pytest.raises(ValueError) as exc:
         Protocol("endpoint", username="username", password="password", read_timeout_sec=30, operation_timeout_sec="29a")
     assert str(exc.value) == "failed to parse operation_timeout_sec as int: " "invalid literal for int() with base 10: '29a'"
+
+def test_fail_set_reconnection_retries():
+    with pytest.raises(ValueError) as exc:
+        Protocol("endpoint", username="username", password="password", reconnection_retries="5a", reconnection_backoff=4.0)
+    assert str(exc.value) == "failed to parse reconnection_retries as int: " "invalid literal for int() with base 10: '5a'"
+
+def test_fail_set_reconnection_backoff():
+    with pytest.raises(ValueError) as exc:
+        Protocol("endpoint", username="username", password="password", reconnection_retries=5, reconnection_backoff="4a")
+    assert str(exc.value) == "failed to parse reconnection_backoff as float: " "could not convert string to float: '4a'"
